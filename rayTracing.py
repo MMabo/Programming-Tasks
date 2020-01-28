e9b1f4beed4c717f44e45f336c4e468274eb1966
MMabo <57180306+MMabo@users.noreply.github.com>
2020-01-28 10:40:58 +0000

    Add files via upload

---
+++ rayTracing.py
@@ -0,0 +1,18 @@
+##Your teacher: Problem - https://projecteuler.net/problem=102
+##Your teacher: Solution explained - https://www.mathblog.dk/project-euler-102-triangles-contain-origin/
+
+def rayCastingAlgorithm(coordinates):
+    A = coordinates[0]
+    B = coordinates[1]
+    C = coordinates[2]
+
+    for i in toInfinity():
+        
+        
+def toInfinity():
+    num = 0
+    while True:
+        num += 1
+        yield num
+
+rayCastingAlgorithm([1,2,3])
